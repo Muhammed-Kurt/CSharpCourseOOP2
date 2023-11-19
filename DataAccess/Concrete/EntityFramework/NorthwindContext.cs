69ef2b08944b64dd8d79a6a47b40e979d5cf327c
Muhammed KURT <muhammed_kurtt@hotmail.com>
2023-11-19 22:33:19 +0300

    Data Access Layer

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -13,12 +13,14 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server = (localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server =(localdb)\KURT;Database=NORTHWND;Trusted_Connection=true");
             
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -77,5 +78,9 @@ namespace DataAccess.Concrete.InMemory
             throw new NotImplementedException();
         }
 
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

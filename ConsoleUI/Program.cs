8b328aa6643c46acbdf868744d41923f716a0eff
Muhammed KURT <muhammed_kurtt@hotmail.com>
2023-11-19 22:33:01 +0300

    Console UI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,46 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            //ProductTest();
+            //CategoryTest();
+            //IoC
+            //DTO = Data Transformation Object
+            ProductGetByUnitPriceTest();
+        }
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+        private static void ProductGetByUnitPriceTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetByUnitPrice(50, 100);
+
+            foreach (var product in result.Data)
             {
                 Console.WriteLine(product.ProductName);
             }
+
+            Console.WriteLine(result.Message);
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetProductDetails();
+            foreach (var product in result.Data)
+            {
+                Console.WriteLine(product.ProductName+" - " + product.CategoryName);
+            }
+
+            Console.WriteLine(result.Message);
         }
     }
 }

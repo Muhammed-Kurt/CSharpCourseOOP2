e3be62adb8816b79c2fe567bb03115713048bf05
Muhammed KURT <muhammed_kurtt@hotmail.com>
2023-11-19 22:34:08 +0300

    ASP.NET WebAPI

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]//--> Attribute --> Bir class ile ilgili bilgi verme imzalama yöntemidir.
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //naming convention
+        //IoC Container -- Inversion of Control
+
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger
+            //Dependency chain -- 
+            var result = _productService.GetAll();
+            if (result.Success==true)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success == true)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success == true)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add serices to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Autofac, Ninject,CastleWindsor,SwtructureMap,LightInject, DryInject --> IoC Container
+            //AOP 
+            services.AddControllers();
+            services.AddSingleton<IProductService, ProductManager>();
+            //AddSingleton = Arka planda bir referans oluştur. Ancak içerisinde veri tutulmuyorsa
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+    }
+}
